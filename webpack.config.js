3476d953fc2a2d6d06859d1f31b6295c0464c4ab
Fabien Rassinier <frassinier@talend.com>
2016-08-23 12:09:01 +0200

    Add some basics examples of Bootstrap override

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const extractBootstrap = new ExtractTextPlugin('bootstrap.css', {
+    allChunks: true,
+});
+const extractBootstrapMin = new ExtractTextPlugin('bootstrap.min.css', {
+    allChunks: true,
+});
+const BOOT_PATH = `${__dirname}/node_modules/bootstrap-sass/assets/stylesheets`;
+const THEME_PATH = `${__dirname}./src`;
+
+const BASE_CONF = {
+    entry: './src/index.js',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bootstrap.js',
+    },
+    resolve: ['', '.scss', '.css', '.js'],
+    module: {
+        loaders: [{
+            test: /index\.scss$/,
+            loader: extractBootstrap.extract('style', 'css!sass'),
+        }],
+    },
+    sassLoader: {
+        includePaths: [THEME_PATH, BOOT_PATH],
+    },
+    plugins: [
+        extractBootstrap,
+        new DashboardPlugin(),
+    ],
+    devServer: {
+        contentBase: './node_modules/bootstrap-tlnd-theme/example',
+    },
+};
+
+const MINIFIED = Object.assign({}, BASE_CONF, {
+    module: {
+        loaders: [{
+            test: /index\.scss$/,
+            loader: extractBootstrapMin.extract('style', 'css?minimize!sass'),
+        }],
+    },
+    plugins: [extractBootstrapMin],
+});
+
+module.exports = [BASE_CONF, MINIFIED];
